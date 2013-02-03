31d282752a884cdc7872e8ae1639f9135c556663
Cornelius Schumacher <schumacher@kde.org>
2013-02-03 14:55:32 +0100

    rails new yalta

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yalta::Application.config.secret_token = '6fb6375296c5b382979c20cd83c801799bdd1072f3a7264d577860b8f83eeb23ad65fdfeed3b3bb26733ba90c38fd4e3fa7dfd54542168a03c05e56002e5bfed'
